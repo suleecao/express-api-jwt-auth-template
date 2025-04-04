7b326862cde5e22c135ac35f0bf2a4fd2df045ce
Teddy <ted.j.gallion@gmail.com>
2025-04-04 15:44:59 -0500

    imports controllers for cocktails routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// npm
 const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
@@ -5,36 +6,32 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
-//step 2a
-const testJwtRouter = require('./controllers/test-jwt');
-//step 9D
+
+// Import routers
 const authRouter = require('./controllers/auth');
-//19D require
+const testJwtRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
-//23 put middleware here instead as parent instead of route definitions  (child)
-const verifyToken = require('./middleware/verify-token');
+const cocktailsRouter = require('./controllers/cocktails')
+// // Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URI);
 
+// mongoose.connection.on('connected', () => {
+//   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+// });
 
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-//step 10 routes below, check postman then install bcrypt in terminal
+// Routes
 app.use('/auth', authRouter);
-//19E invoke 23B put verifyToken here
-app.use('/users', verifyToken, usersRouter);
-//step 2b, go to postman after this
 app.use('/test-jwt', testJwtRouter);
+app.use('/users', usersRouter);
+app.use("/cocktails", cocktailsRouter);
 
 
-
-
+// Start the server and listen on port 3000
 app.listen(3000, () => {
   console.log('The express app is ready!');
 });
