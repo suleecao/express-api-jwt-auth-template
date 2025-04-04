d87674e78179b597281d43a4a56e5c48e1454e41
Teddy <ted.j.gallion@gmail.com>
2025-04-04 15:41:35 -0500

    adds controllers for cocktails routes

---
+++ controllers/cocktails.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const formatIngredients = require("../utils/format-ingredients");
+router.get("/random", async(req,res) =>{
+	try{
+		const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+		const data = await response.json();
+		const drink = data.drinks[0];
+		const ingredients = formatIngredients(drink)
+		res.json({
+			name: drink.strDrink,
+			instructions: drink.strInstructions,
+      image: drink.strDrinkThumb,
+      ingredients: ingredients,
+		});
+	}catch(error){
+		console.error("error fetching cocktail data: ", error.message);
+		res.status(500).json({error:error.message})
+	}
+
+})
+router.get('/:cocktailName', async (req, res) => {
+  try {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+req.params.cocktailName);
+    const data = await response.json();
+    const drinks = data.drinks;
+    const firstDrink = drinks[0];
+
+    const ingredients = formatIngredients(firstDrink);
+    console.log(ingredients);
+
+    res.json({
+      name: firstDrink.strDrink,
+      instructions: firstDrink.strInstructions,
+      image: firstDrink.strDrinkThumb,
+      ingredients: ingredients,
+    });
+
+  } catch (error) {
+    console.error('Error fetching cocktail data:', error.message);
+    res.status(500).json({ error: 'Failed to fetch cocktail data' });
+  }
+});
+
+module.exports = router;
