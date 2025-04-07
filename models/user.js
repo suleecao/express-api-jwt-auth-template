1787cdb946a75735d86939e8d1cb83b767ea8418
Teddy <ted.j.gallion@gmail.com>
2025-04-07 12:08:51 -0500

    WIP: updates user model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,11 @@
-//define userSchema and userModel
-//step 8 define user and schema
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
         username: String,
-        hashedPassword: String, 
-    
+        hashedPassword: String,     
 });
 
-//step 14 removes the hashed PW from response with a special hook
-//14b go to postman to sign up another user on the sign up page, see how postman
-//removes it from the object in server memory
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         delete returnedObject.hashedPassword;
