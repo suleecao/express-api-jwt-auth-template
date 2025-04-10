6ba17d07550e664ded6d11c74ab09fac43c528be
Teddy <ted.j.gallion@gmail.com>
2025-04-10 13:37:06 -0500

    wip: changes to cocktail model in DB

--- models/cocktail.js
+++ models/cocktail.js
@@ -8,34 +8,34 @@ const Ingredient = mongoose.model("Ingredient", ingredientSchema);
 const glassesArr = ['Highball glass', 'Old-fashioned glass', 'Cocktail glass', 'Copper Mug', 'Whiskey Glass', 'Collins glass', 'Pousse cafe glass', 'Champagne flute', 'Whiskey sour glass', 'Brandy snifter', 'White wine glass', 'Nick and Nora Glass', 'Hurricane glass', 'Coffee mug', 'Shot glass', 'Jar', 'Irish coffee cup', 'Punch bowl', 'Pitcher', 'Pint glass', 'Cordial glass', 'Beer mug', 'Margarita/Coupette glass', 'Beer pilsner', 'Beer Glass', 'Parfait glass', 'Wine Glass', 'Mason jar', 'Margarita glass', 'Martini Glass', 'Balloon Glass', 'Coupe Glass']
 
 const cocktailSchema = new mongoose.Schema({
-	drinkName: {
-    type: String, 
+  drinkName: {
+    type: String,
     required: true
   },
-	creator:{
+  creator: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     default: new mongoose.Types.ObjectId("67f401c1478b7fcee134d8ee"),
-    required:true
-
+    required: true
   },
-	instructions: String,
-	image: String,
-	ingredients: [
-		{
-		  ingredient: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: "Ingredient"
-    },
-		  amount: String 
-		}
-	],
-	glass: {
-		type:String,
-		enum: glassesArr,
-		required:true,
-	},
-	reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }]
+  instructions: String,
+  image: String,
+  ingredients: [
+    {
+      ingredient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ingredient"
+      },
+      amount: String,
+      ingredientName: String 
+    }
+  ],
+  glass: {
+    type: String,
+    enum: glassesArr,
+    required: true,
+  },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }]
 });
 
 const Cocktail = mongoose.model("Cocktail", cocktailSchema);
